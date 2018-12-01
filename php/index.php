aeed555a789f1f53551fffe98d974679ce691650
Julian <julian.reuscher@gmx.de>
2018-12-01 16:24:39 +0100

    HTML Vorlage

---
+++ php/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Übersicht Umfragen</title>
+</head>
+
+<body>
+
+<table>
+    <thead>
+    <tr>
+        <th>Meine Umfragen</th>
+        <th>Share-Links</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Umfrage 1</td>
+        <td>julian.free-poll.de/umfrage1 &#9733;</td>
+    </tr>
+    <tr>
+        <td>Umfrage 2</td>
+        <td>julian.free-poll.de/umfrage2 &#9733;</td>
+    </tr>
+    <tr>
+        <td>Umfrage 3</td>
+        <td>julian.free-poll.de/umfrage3 &#9733;</td>
+    </tr>
+    <tr>
+        <td>Umfrage 4</td>
+        <td>julian.free-poll.de/umfrage4 &#9733;</td>
+    </tr>
+    </tbody>
+    <tfoot>
+    <tr>
+        <td colspan="7">Share-Links können durch einen Klick auf den Stern personalisiert werden.</td>
+    </tr>
+    </tfoot>
+</table>
+
+
+</body>
+</html>
