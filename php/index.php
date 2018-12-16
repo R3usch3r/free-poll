a1abd60879b07d911280fc1cb817e88b36f42634
Julian <julian.reuscher@gmx.de>
2018-12-16 20:55:22 +0100

    HTML Vorlage

--- php/index.php
+++ php/index.php
@@ -24,8 +24,11 @@ if (!$conn) {
 
 echo "Connected successfully";
 
+<input type="text" name="slink" value="<?php echo $token; ?>">
+<input type="submit">
 $token = $_POST['wasauchimmer'];
-$neuerWert = $_POST['asdf'];
+$neuerWert = $_POST['slink'];
+
 
 // New Value for Link
 $sql = "UPDATE polls SET url='$neuerWert' WHERE url='$token'";
@@ -35,39 +38,6 @@ if (mysqli_query($con, $sql)) {
     echo "Error updating record: " . mysqli_error($con);
 }
 ?>
-
-<table>
-    <thead>
-    <tr>
-        <th>Meine Umfragen</th>
-        <th>Share-Links</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-        <td>Umfrage 1</td>
-        <td>julian.free-poll.de/umfrage1 &#9733;</td>
-    </tr>
-    <tr>
-        <td>Umfrage 2</td>
-        <td>julian.free-poll.de/umfrage2 &#9733;</td>
-    </tr>
-    <tr>
-        <td>Umfrage 3</td>
-        <td>julian.free-poll.de/umfrage3 &#9733;</td>
-    </tr>
-    <tr>
-        <td>Umfrage 4</td>
-        <td>julian.free-poll.de/umfrage4 &#9733;</td>
-    </tr>
-    </tbody>
-    <tfoot>
-    <tr>
-        <td colspan="7">Share-Links können durch einen Klick auf den Stern personalisiert werden.</td>
-    </tr>
-    </tfoot>
-</table>
-
 
 </body>
 </html>
